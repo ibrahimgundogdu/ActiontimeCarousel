1e0a334f1bc54d1f563351874c2018a440e1676c
İbrahim Gündoğdu <ibrahim.gundogdu@gmail.com>
2019-11-21 11:47:12 +0300

    ufeservice

--- ActionForce/ActionForce.Integration/UfeService/UfeServiceClient.cs
+++ ActionForce/ActionForce.Integration/UfeService/UfeServiceClient.cs
@@ -10,7 +10,7 @@ namespace ActionForce.Integration.UfeService
 {
     public class UfeServiceClient
     {
-        public const string apiUrl = "https://ufeservice.ufegrup.com";
+        public const string apiUrl = "http://ufeservice.ufegrup.com";
 
         private RestClient Client = new RestClient(apiUrl);
 
