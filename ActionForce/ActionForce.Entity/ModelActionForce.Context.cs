5e30755c50cbb43c0bee3152a25c42f25a8e1316
Selim Dinçer <csharpsoftware@hotmail.com>
2019-11-22 09:59:47 +0300

    sdfsdfsdf

--- ActionForce/ActionForce.Entity/ModelActionForce.Context.cs
+++ ActionForce/ActionForce.Entity/ModelActionForce.Context.cs
@@ -210,6 +210,7 @@ namespace ActionForce.Entity
         public virtual DbSet<VDocumentPosCancel> VDocumentPosCancel { get; set; }
         public virtual DbSet<VDocumentPosRefund> VDocumentPosRefund { get; set; }
         public virtual DbSet<LocationPosTerminal> LocationPosTerminal { get; set; }
+        public virtual DbSet<VLocationSchedule> VLocationSchedule { get; set; }
     
         public virtual int AddApplicationLog(string environment, string modul, string processType, string processID, string controller, string action, string tableName, string fieldName, string oldValue, string newValue, Nullable<bool> isSuccess, string resultMessage, string errorMessage, Nullable<System.DateTime> recordDate, string recordEmployee, string recordIP, string recordDevice)
         {
