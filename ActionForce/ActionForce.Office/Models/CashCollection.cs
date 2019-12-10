7b73877a902a2c4cb2f484ee26804c7dc5472f4c
İbrahim Gündoğdu <ibrahim.gundogdu@gmail.com>
2019-12-10 16:00:33 +0300

    document manager

---
+++ ActionForce/ActionForce.Office/Models/CashCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActionForce.Office
+{
+    public class CashCollection
+    {
+        public int ActinTypeID { get; set; }
+        public int? FromEmployeeID { get; set; }
+        public int? FromBankAccountID { get; set; }
+        public int? FromCustomerID { get; set; }
+        public int LocationID { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+        public DateTime? DocumentDate { get; set; }
+        public string Description { get; set; }
+        public double ExchangeRate { get; set; }
+        public int? EnvironmentID { get; set; }
+    }
+}
