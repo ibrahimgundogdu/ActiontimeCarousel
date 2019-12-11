e8cfc000d7f19bdb9756d2db0197a69e710bf7dd
İbrahim Gündoğdu <ibrahim.gundogdu@gmail.com>
2019-12-11 19:59:22 +0300

    shift sayfası tasarımı

--- ActionForce/ActionForce.Office/Controllers/ShiftController.cs
+++ ActionForce/ActionForce.Office/Controllers/ShiftController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ActionForce.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +9,60 @@ namespace ActionForce.Office.Controllers
 {
     public class ShiftController : BaseController
     {
-        public ActionResult Index()
+        [AllowAnonymous]
+        public ActionResult Index(string date)
         {
             ShiftControlModel model = new ShiftControlModel();
 
+
+            if (TempData["result"] != null)
+            {
+                model.Result = TempData["result"] as Result<EmployeeShift> ?? null;
+            }
+
+            var _date = DateTime.UtcNow.AddHours(model.Authentication.ActionEmployee.OurCompany.TimeZone.Value).Date;
+            var datekey = Db.DateList.FirstOrDefault(x => x.DateKey == _date);
+
+            if (!string.IsNullOrEmpty(date))
+            {
+                _date = Convert.ToDateTime(date).Date;
+                datekey = Db.DateList.FirstOrDefault(x => x.DateKey == _date);
+            }
+
+            model.CurrentDate = datekey;
+            model.TodayDateCode = DateTime.UtcNow.AddHours(model.Authentication.ActionEmployee.OurCompany.TimeZone.Value).Date.ToString("yyyy-MM-dd");
+            model.CurrentDateCode = _date.ToString("yyyy-MM-dd");
+            model.PrevDateCode = _date.AddDays(-1).Date.ToString("yyyy-MM-dd");
+            model.NextDateCode = _date.AddDays(1).Date.ToString("yyyy-MM-dd");
+
+            model.CurrentCompany = Db.OurCompany.FirstOrDefault(x => x.CompanyID == model.Authentication.ActionEmployee.OurCompanyID);
+            model.Locations = Db.Location.Where(x => x.OurCompanyID == model.Authentication.ActionEmployee.OurCompanyID && x.IsActive == true).OrderBy(x => x.SortBy).ToList();
+
+            List<int> locationids = model.Locations.Select(x => x.LocationID).ToList();
+
+            model.LocationSchedules = Db.LocationSchedule.Where(x => locationids.Contains(x.LocationID.Value) && x.ShiftDate == datekey.DateKey && x.StatusID == 2).ToList();
+            model.LocationShifts = Db.LocationShift.Where(x => locationids.Contains(x.LocationID) && x.ShiftDate == datekey.DateKey).ToList();
+
+            model.EmployeeSchedules = Db.Schedule.Where(x => locationids.Contains(x.LocationID.Value) && x.ShiftDate == model.CurrentDate.DateKey).ToList();
+            List<int> employeeids = model.EmployeeSchedules.Select(x => x.EmployeeID.Value).ToList();
+
+            model.Employees = Db.Employee.Where(x => employeeids.Contains(x.EmployeeID) && x.IsActive == true).ToList();
+
+            model.EmployeeShifts = Db.EmployeeShift.Where(x => x.ShiftDate == model.CurrentDate.DateKey && x.IsWorkTime == true).ToList();
+            model.EmployeeBreaks = Db.EmployeeShift.Where(x => x.ShiftDate == model.CurrentDate.DateKey && x.IsBreakTime == true).ToList();
+
             return View(model);
         }
+
+        [AllowAnonymous]
         public ActionResult LocationShift()
         {
             ShiftControlModel model = new ShiftControlModel();
 
             return View(model);
         }
+
+        [AllowAnonymous]
         public ActionResult EmployeeShift()
         {
             ShiftControlModel model = new ShiftControlModel();
--- ActionForce/ActionForce.Office/Models/ShiftControlModel.cs
+++ ActionForce/ActionForce.Office/Models/ShiftControlModel.cs
@@ -9,7 +9,25 @@ namespace ActionForce.Office
     public class ShiftControlModel : LayoutControlModel
     {
         public IEnumerable<Location> Locations { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
+
         public IEnumerable<LocationShift> LocationShifts { get; set; }
+        public IEnumerable<LocationSchedule> LocationSchedules { get; set; }
+
         public IEnumerable<EmployeeShift> EmployeeShifts { get; set; }
+        public IEnumerable<EmployeeShift> EmployeeBreaks { get; set; }
+        public IEnumerable<Schedule> EmployeeSchedules { get; set; }
+
+        public FilterModel Filters { get; set; }
+        public OurCompany CurrentCompany { get; set; }
+        public Location CurrentLocation { get; set; }
+        public DateList CurrentDate { get; set; }
+        public Result<EmployeeShift> Result { get; set; }
+
+
+        public string TodayDateCode { get; set; }
+        public string CurrentDateCode { get; set; }
+        public string NextDateCode { get; set; }
+        public string PrevDateCode { get; set; }
     }
 }
