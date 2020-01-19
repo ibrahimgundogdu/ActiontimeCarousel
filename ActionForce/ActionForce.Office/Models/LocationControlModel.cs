7bb22b790c572274bef9f1f27031e1a5ba10feaf
Metin Günay <metingunay_3490@hotmail.com>
2020-01-19 18:59:52 +0300

    - Lokasyon işlemleri geliştirme devam.

--- ActionForce/ActionForce.Office/Models/LocationControlModel.cs
+++ ActionForce/ActionForce.Office/Models/LocationControlModel.cs
@@ -10,8 +10,39 @@ namespace ActionForce.Office
     {
         public Result Result { get; set; }
 
-        public List<GetLocationAll_Result> LocationList { get; set; }
+        public List<LocationModel> LocationList { get; set; }
+        public LocationModel LocationModel { get; set; }
         public List<string> StateList { get; set; }
         public List<string> TypeList { get; set; }
+        public LocationFilterModel FilterModel { get; set; }
+    }
+
+    public class LocationFilterModel
+    {
+        public string LocationName { get; set; }
+        public string State { get; set; }
+        public string TypeName { get; set; }
+        public int IsActive { get; set; }
+    }
+
+    public class LocationModel
+    {
+        public int LocationID { get; set; }
+        public string LocationCode { get; set; }
+        public string LocationName { get; set; }
+        public string LocationNameSearch { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public Nullable<int> Timezone { get; set; }
+        public string MapURL { get; set; }
+        public Nullable<bool> IsHaveOperator { get; set; }
+        public string IP { get; set; }
+        public string SortBy { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public string TypeName { get; set; }
+        public string Currency { get; set; }
+        public Nullable<System.Guid> LocationUID { get; set; }
     }
 }
