858823f3b335ba7862afbd6124cb912f81f41136
İbrahim Gündoğdu <ibrahim.gundogdu@gmail.com>
2020-01-31 19:05:22 +0300

    Compute sayfası eklendi

---
+++ ActionForce/ActionForce.Office/Controllers/ComputeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionForce.Office.Controllers
+{
+    public class ComputeController : BaseController
+    {
+        // GET: Compute
+        public ActionResult Index()
+        {
+            ComputeControlModel model = new ComputeControlModel();
+            model.Locations = Db.Location.Where(x => x.OurCompanyID == model.Authentication.ActionEmployee.OurCompanyID).ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ ActionForce/ActionForce.Office/Models/ControlModels/ComputeControlModel.cs
@@ -0,0 +1,18 @@
+﻿using ActionForce.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActionForce.Office
+{
+    public class ComputeControlModel : LayoutControlModel
+    {
+        public Result Result { get; set; }
+        public IEnumerable<Location> Locations { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
+        public IEnumerable<DateList> DateLists { get; set; }
+        public int WeekYear { get; set; }
+        public int WeekNumber { get; set; }
+    }
+}
