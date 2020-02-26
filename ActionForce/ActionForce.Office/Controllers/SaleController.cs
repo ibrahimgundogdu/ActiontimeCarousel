ed6063982d1ee667ce0e48f1b25090796955d32e
İbrahim Gündoğdu <ibrahim.gundogdu@gmail.com>
2020-02-26 15:41:41 +0300

    price category

--- ActionForce/ActionForce.Office/Controllers/SaleController.cs
+++ ActionForce/ActionForce.Office/Controllers/SaleController.cs
@@ -32,7 +32,7 @@ namespace ActionForce.Office.Controllers
         {
             SaleControlModel model = new SaleControlModel();
 
-            model.PriceCategoryList = Db.VPriceCategory.ToList();
+            model.PriceCategoryList = Db.VPriceCategory.Where(x => x.OurCompanyID == model.Authentication.ActionEmployee.OurCompanyID).ToList();
             model.OurCompanyList = Db.OurCompany.ToList();
 
             return View(model);
@@ -48,7 +48,6 @@ namespace ActionForce.Office.Controllers
 
             return PartialView("_PartialAddPriceCategory", model);
         }
-
 
         [HttpPost]
         [AllowAnonymous]
@@ -84,11 +83,12 @@ namespace ActionForce.Office.Controllers
                 OfficeHelper.AddApplicationLog("Office", "PriceCategory", "Insert", cat.ID.ToString(), "Sale", "AddPriceCategory", null, true, $"{model.Result.Message}", string.Empty, DateTime.UtcNow.AddHours(3), model.Authentication.ActionEmployee.FullName, OfficeHelper.GetIPAddress(), string.Empty, cat);
             }
 
-            model.PriceCategoryList = Db.VPriceCategory.ToList();
+            model.PriceCategoryList = Db.VPriceCategory.Where(x => x.OurCompanyID == model.Authentication.ActionEmployee.OurCompanyID).ToList();
             model.OurCompanyList = Db.OurCompany.ToList();
 
             return PartialView("_PartialPriceCategoryList", model);
         }
+
 
         [HttpPost]
         [AllowAnonymous]
@@ -96,10 +96,119 @@ namespace ActionForce.Office.Controllers
         {
             SaleControlModel model = new SaleControlModel();
 
-            model.PriceCategory = Db.VPriceCategory.FirstOrDefault(x=> x.ID == id);
+            model.PriceCategory = Db.VPriceCategory.FirstOrDefault(x => x.ID == id);
             model.OurCompanyList = Db.OurCompany.ToList();
 
             return PartialView("_PartialEditPriceCategory", model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public PartialViewResult EditPriceCategory(CUPriceCategory pricecategory)
+        {
+            SaleControlModel model = new SaleControlModel();
+
+            model.Result = new Result()
+            {
+                IsSuccess = false,
+                Message = string.Empty
+            };
+
+            if (pricecategory != null)
+            {
+                PriceCategory cat = Db.PriceCategory.FirstOrDefault(x => x.ID == pricecategory.ID);
+
+                if (cat != null)
+                {
+
+                    PriceCategory self = new PriceCategory()
+                    {
+                        CategoryCode = cat.CategoryCode,
+                        CategoryName = cat.CategoryName,
+                        ID = cat.ID,
+                        IsActive = cat.IsActive,
+                        IsMaster = cat.IsMaster,
+                        OurCompanyID = cat.OurCompanyID,
+                        RecordDate = cat.RecordDate,
+                        RecordEmployeeID = cat.RecordEmployeeID,
+                        RecordIP = cat.RecordIP,
+                        SortBy = cat.SortBy,
+                        UpdateDate = cat.UpdateDate,
+                        UpdateEmployeeID = cat.UpdateEmployeeID,
+                        UpdateIP = cat.UpdateIP
+                    };
+
+
+                    cat.CategoryCode = pricecategory.CategoryCode;
+                    cat.CategoryName = pricecategory.CategoryName;
+                    cat.IsMaster = !string.IsNullOrEmpty(pricecategory.IsMaster) && pricecategory.IsMaster == "1" ? true : false;
+                    cat.IsActive = !string.IsNullOrEmpty(pricecategory.IsActive) && pricecategory.IsActive == "1" ? true : false;
+                    cat.OurCompanyID = pricecategory.OurCompanyID;
+                    cat.RecordDate = DateTime.Now;
+                    cat.RecordEmployeeID = model.Authentication.ActionEmployee.EmployeeID;
+                    cat.RecordIP = OfficeHelper.GetIPAddress();
+                    cat.SortBy = pricecategory.SortBy;
+
+                    Db.SaveChanges();
+
+                    model.Result.IsSuccess = true;
+                    model.Result.Message = $"{cat.CategoryName} Fiyat kategorisi güncellendi.";
+
+                    var isequal = OfficeHelper.PublicInstancePropertiesEqual<PriceCategory>(self, cat, OfficeHelper.getIgnorelist());
+                    OfficeHelper.AddApplicationLog("Office", "PriceCategory", "Update", cat.ID.ToString(), "Sale", "EditPriceCategory", isequal, true, $"{model.Result.Message}", string.Empty, DateTime.UtcNow.AddHours(3), model.Authentication.ActionEmployee.FullName, OfficeHelper.GetIPAddress(), string.Empty, null);
+
+                }
+                else
+                {
+                    model.Result.Message = $"{pricecategory.ID} ID'li fiyat kategorisi bulunamadı.";
+                }
+            }
+
+            TempData["result"] = model.Result;
+
+            model.PriceCategoryList = Db.VPriceCategory.Where(x => x.OurCompanyID == model.Authentication.ActionEmployee.OurCompanyID).ToList();
+            model.OurCompanyList = Db.OurCompany.ToList();
+
+            return PartialView("_PartialPriceCategoryList", model);
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult PartialRemovePriceCategory(int? id)
+        {
+            SaleControlModel model = new SaleControlModel();
+
+            model.Result = new Result();
+
+            var pricecat = Db.PriceCategory.FirstOrDefault(x => x.ID == id);
+            var pricescount = Db.Price.Where(x => x.PriceCategoryID == id)?.Count();
+
+            if (pricescount <= 0 && pricecat != null)
+            {
+                model.Result.IsSuccess = true;
+                model.Result.Message = "Kategori başarı ile silindi";
+
+                OfficeHelper.AddApplicationLog("Office", "PriceCategory", "Delete", pricecat.ID.ToString(), "Sale", "PartialRemovePriceCategory", null, true, $"{model.Result.Message}", string.Empty, DateTime.UtcNow.AddHours(3), $"{model.Authentication.ActionEmployee.EmployeeID} {model.Authentication.ActionEmployee.FullName}", OfficeHelper.GetIPAddress(), string.Empty, pricecat);
+
+                Db.PriceCategory.Remove(pricecat);
+                Db.SaveChanges();
+            }
+            else
+            {
+                if (pricescount > 0)
+                {
+                    model.Result.IsSuccess = false;
+                    model.Result.Message = "Kategori silinemedi. Bağlı fiyatlar var";
+                }
+            }
+
+            model.PriceCategoryList = Db.VPriceCategory.Where(x => x.OurCompanyID == model.Authentication.ActionEmployee.OurCompanyID).ToList();
+            TempData["result"] = model.Result;
+
+            return PartialView("_PartialPriceCategoryList", model);
+
+
         }
 
         #endregion
