739f4ce311327ad5abc17625a594e996d27038ba
Ebru Kaman <ebrukamann@gmail.com>
2020-03-10 18:37:33 +0300

    MallList Oluşturuldu

--- ActionForce/ActionForce.Office/Controllers/MallController.cs
+++ ActionForce/ActionForce.Office/Controllers/MallController.cs
@@ -13,6 +13,19 @@ namespace ActionForce.Office.Controllers
         {
             MallControlModel model = new MallControlModel();
 
+            model.MallList = Db.VMall.Where(x => x.OurCompanyID == model.Authentication.ActionEmployee.OurCompanyID).ToList();
+            model.StateList = model.MallList.Select(x => x.StateName).Distinct().OrderBy(x => x).ToList();
+            model.CityList = model.MallList.Select(x => x.CityName).Distinct().OrderBy(x => x).ToList();
+            model.CountryList = model.MallList.Select(x => x.CountryName).Distinct().OrderBy(x => x).ToList();
+
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult MallFilter(MallFilterModel getfilterMall)
+        {
             return View();
         }
     }
--- ActionForce/ActionForce.Office/Models/ControlModels/MallControlModel.cs
+++ ActionForce/ActionForce.Office/Models/ControlModels/MallControlModel.cs
@@ -9,6 +9,12 @@ namespace ActionForce.Office
     public class MallControlModel:LayoutControlModel
     {
         public List<VMall> MallList { get; set; }
+        public List<MallLocationContract> LocationContracts { get; set; }
+        public List<string> StateList { get; set; }
+        public List<string> CityList { get; set; }
+        public List<string> CountryList { get; set; }
         public Result Result { get; set; }
+        public MallFilterModel FilterModel { get; set; }
+
     }
 }
