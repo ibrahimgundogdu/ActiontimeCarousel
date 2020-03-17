b28c85d048b0d4cb18ed7e5cd882d5206ddbccc1
İbrahim Gündoğdu <ibrahim.gundogdu@gmail.com>
2020-03-17 16:11:51 +0300

    model

--- ActionForce/ActionForce.Location/Controllers/DefaultController.cs
+++ ActionForce/ActionForce.Location/Controllers/DefaultController.cs
@@ -231,7 +231,18 @@ namespace ActionForce.Location.Controllers
         {
             DefaultControlModel model = new DefaultControlModel();
 
-            
+            if (neworder != null)
+            {
+                DateTime date = DateTime.UtcNow.AddHours(model.Location.Timezone.Value).Date;
+
+                var OrderNumber = string.Format("S{0}{1}{2}{3}", date.Year.ToString().Substring(2, 2), date.Month < 10 ? "0" + date.Month.ToString() : date.Month.ToString(), date.Day < 10 ? "0" + date.Day.ToString() : date.Day.ToString(), DateTime.Now.Ticks.ToString());
+
+                var orderID = Db.AddOrder(model.Authentication.CurrentLocation.ID, model.Authentication.CurrentEmployee.EmployeeID, OrderNumber, neworder.OrderFullName, neworder.OrderDetail, neworder.OrderMobile, neworder.OrderIdentity, neworder.OrderDescription, LocationHelper.GetIPAddress());
+
+                LocationHelper.AddApplicationLog("Location", "TicketSale", "Insert", orderID.ToString(), "Default", "CreateOrder", null, true, $"{OrderNumber} Nolu Sipariş Eklendi", string.Empty, DateTime.UtcNow.AddHours(model.Location.Timezone.Value), $"{model.Authentication.CurrentEmployee.EmployeeID} {model.Authentication.CurrentEmployee.FullName}", LocationHelper.GetIPAddress(), string.Empty, null);
+
+            }
+
 
             return PartialView("_PartialAddOrderInfo", model);
         }
