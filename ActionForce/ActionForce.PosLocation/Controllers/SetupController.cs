234ed7c3123864e58de1f426ee695820bd30d7cc
İbrahim Gündoğdu <ibrahim.gundogdu@gmail.com>
2021-09-07 00:05:38 +0300

    giriş cookie kısımları ok

--- ActionForce/ActionForce.PosLocation/Controllers/SetupController.cs
+++ ActionForce/ActionForce.PosLocation/Controllers/SetupController.cs
@@ -12,89 +12,113 @@ namespace ActionForce.PosLocation.Controllers
         public ActionResult Index()
         {
             SetupControlModel model = new SetupControlModel();
+
+            Response.Cookies.Remove("PosTerminal");
+            Response.Cookies.Remove("PosLocation");
+            Response.Cookies.Remove("AuthenticationToken");
+
             if (TempData["Result"] != null)
             {
                 model.Result = (Result)TempData["Result"];
             }
 
-            model.LocationList = Db.Location.Where(x => x.OurCompanyID == 2 & x.IsActive == true).OrderBy(x => x.SortBy).ToList();
             return View(model);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
-        public ActionResult SetLocation(string LocationUID)
+        public ActionResult SetLocation(string SicilNumber)
         {
             SetupControlModel model = new SetupControlModel();
+            model.Result = new Result() {
+                IsSuccess = false,
+                Message = string.Empty
+            };
 
-            Response.Cookies.Remove("PosLocation");
+            //Response.Cookies.Remove("PosLocation");
 
-            HttpCookie locationCookie = System.Web.HttpContext.Current.Request.Cookies["PosLocation"];
+            //HttpCookie locationCookie = System.Web.HttpContext.Current.Request.Cookies["PosLocation"];
 
-            if (locationCookie == null)
-                locationCookie = new HttpCookie("PosLocation");
+            //if (locationCookie == null)
+            //    locationCookie = new HttpCookie("PosLocation");
 
-            locationCookie.Value = LocationUID.ToString();
-            locationCookie.Expires = DateTime.Now.AddYears(10);
+            //locationCookie.Value = LocationUID.ToString();
+            //locationCookie.Expires = DateTime.Now.AddYears(10);
 
-            Response.SetCookie(locationCookie);
+            //Response.SetCookie(locationCookie);
 
+            var posTerminal = Db.VLocationPosTerminal.Where(x => (x.SicilNumber == SicilNumber  || x.SerialNumber == SicilNumber) && x.IsMaster == true && x.IsActive == true).OrderByDescending(x=> x.RecordDate).FirstOrDefault();
 
-            return RedirectToAction("Serial");
-        }
-
-        public ActionResult Serial()
-        {
-            SetupControlModel model = new SetupControlModel();
-            if (TempData["Result"] != null)
+            if (posTerminal != null)
             {
-                model.Result = (Result)TempData["Result"];
-            }
-
-            HttpCookie locationCookie = System.Web.HttpContext.Current.Request.Cookies["PosLocation"];
-
-            if (locationCookie != null && !string.IsNullOrEmpty(locationCookie.Value))
-            {
-                var LocationUID = locationCookie.Value;
-
-                model.Location = Db.Location.FirstOrDefault(x => x.LocationUID.ToString() == LocationUID & x.IsActive == true);
-
-                if (model.Location != null)
-                {
-                    var posTerminal = Db.VLocationPosTerminal.FirstOrDefault(x => x.LocationID == model.Location.LocationID && x.IsMaster == true && x.IsActive == true);
-
-                    if (posTerminal != null)
-                    {
-
-
-                        //// bunu kullancı doğrularsa
-                        //Response.Cookies.Remove("PosTerminal");
-
-                        //HttpCookie locationPosCookie = System.Web.HttpContext.Current.Request.Cookies["PosTerminal"];
-
-                        //if (locationPosCookie == null)
-                        //    locationPosCookie = new HttpCookie("PosTerminal");
-
-                        //locationPosCookie.Value = posTerminal.SerialNumber.ToString();
-                        //locationPosCookie.Expires = DateTime.Now.AddYears(10);
-
-                        //Response.SetCookie(locationPosCookie);
-
-                        model.PosTerminalSerial = posTerminal.SerialNumber;
-
-                    }
-                }
+                model.Result.IsSuccess = true;
+                model.Result.Message = "Lokasyon Bulundu!";
+                model.Location = Db.Location.FirstOrDefault(x => x.LocationID == posTerminal.LocationID);
+                model.PosTerminalSerial = posTerminal.SicilNumber;
             }
             else
             {
-                return RedirectToAction("Index");
+                model.Result.IsSuccess = false;
+                model.Result.Message = "Sicil Numarasına Ait Bir Lokasyon Bulunamadı!";
+                model.PosTerminalSerial = SicilNumber;
             }
-
-
+            
 
             return View(model);
         }
+
+        //public ActionResult Serial()
+        //{
+        //    SetupControlModel model = new SetupControlModel();
+        //    if (TempData["Result"] != null)
+        //    {
+        //        model.Result = (Result)TempData["Result"];
+        //    }
+
+        //    HttpCookie locationCookie = System.Web.HttpContext.Current.Request.Cookies["PosLocation"];
+
+        //    if (locationCookie != null && !string.IsNullOrEmpty(locationCookie.Value))
+        //    {
+        //        var LocationUID = locationCookie.Value;
+
+        //        model.Location = Db.Location.FirstOrDefault(x => x.LocationUID.ToString() == LocationUID & x.IsActive == true);
+
+        //        if (model.Location != null)
+        //        {
+        //            var posTerminal = Db.VLocationPosTerminal.FirstOrDefault(x => x.LocationID == model.Location.LocationID && x.IsMaster == true && x.IsActive == true);
+
+        //            if (posTerminal != null)
+        //            {
+
+
+        //                //// bunu kullancı doğrularsa
+        //                //Response.Cookies.Remove("PosTerminal");
+
+        //                //HttpCookie locationPosCookie = System.Web.HttpContext.Current.Request.Cookies["PosTerminal"];
+
+        //                //if (locationPosCookie == null)
+        //                //    locationPosCookie = new HttpCookie("PosTerminal");
+
+        //                //locationPosCookie.Value = posTerminal.SerialNumber.ToString();
+        //                //locationPosCookie.Expires = DateTime.Now.AddYears(10);
+
+        //                //Response.SetCookie(locationPosCookie);
+
+        //                model.PosTerminalSerial = posTerminal.SicilNumber;
+
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+
+
+
+        //    return View(model);
+        //}
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -103,28 +127,45 @@ namespace ActionForce.PosLocation.Controllers
         {
             SetupControlModel model = new SetupControlModel();
 
-            if (form.Correct == 1)
-            {
-
-                Response.Cookies.Remove("PosTerminal");
-
-                HttpCookie locationPosCookie = System.Web.HttpContext.Current.Request.Cookies["PosTerminal"];
-
-                if (locationPosCookie == null)
-                    locationPosCookie = new HttpCookie("PosTerminal");
-
-                locationPosCookie.Value = form.PosTerminalSerial;
-                locationPosCookie.Expires = DateTime.Now.AddYears(10);
-
-                Response.SetCookie(locationPosCookie);
-            }
-
             if (form.Fail == 1)
             {
 
                 Response.Cookies.Remove("PosTerminal");
-                return RedirectToAction("Serial");
+                Response.Cookies.Remove("PosLocation");
+                Response.Cookies.Remove("AuthenticationToken");
+
+                return RedirectToAction("Index");
             }
+
+            var location = Db.Location.FirstOrDefault(x => x.LocationID == form.LocationID);
+
+            if (location != null)
+            {
+                Response.Cookies.Remove("PosLocation");
+
+                HttpCookie locationCookie = System.Web.HttpContext.Current.Request.Cookies["PosLocation"];
+
+                if (locationCookie == null)
+                    locationCookie = new HttpCookie("PosLocation");
+
+                locationCookie.Value = location.LocationUID.ToString();
+                locationCookie.Expires = DateTime.Now.AddYears(10);
+
+                Response.SetCookie(locationCookie);
+            }
+
+
+            Response.Cookies.Remove("PosTerminal");
+
+            HttpCookie locationPosCookie = System.Web.HttpContext.Current.Request.Cookies["PosTerminal"];
+
+            if (locationPosCookie == null)
+                locationPosCookie = new HttpCookie("PosTerminal");
+
+            locationPosCookie.Value = form.PosTerminalSerial;
+            locationPosCookie.Expires = DateTime.Now.AddYears(10);
+
+            Response.SetCookie(locationPosCookie);
 
             return RedirectToAction("Employee");
 
